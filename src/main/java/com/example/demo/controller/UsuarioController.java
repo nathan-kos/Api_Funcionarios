6a74ce3c4afdc95998a8a961cd9a30f2be2308d2
nathan ks <nathanks0210@gmail.com>
2023-01-23 22:47:10 -0300

    consulta de usuario byid

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,19 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
     
     @GetMapping(path = "/api/usuario/{id}")
     public ResponseEntity<Usuario> Consultar(@PathVariable("id") String id) {
-        return null;
+        return repository.findById(Long.parseLong(id))
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
-
 
 }
