da50bd8c9104e3d19074e3528fca3badf0d9009e
nathan ks <nathanks0210@gmail.com>
2023-01-23 22:25:16 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "nome" , length = 50)
+    private String nome;
+    @Column(name = "salario")
+    private long salario;
+    @Column(name = "cargo", length = 50)
+    private String cargo;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public long getSalario() {
+        return salario;
+    }
+    public void setSalario(long salario) {
+        this.salario = salario;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    
+
+}
