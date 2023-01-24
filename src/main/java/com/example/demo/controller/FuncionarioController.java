098756a642eecd5a1ffb65981a03188f21553c07
nathan ks <nathanks0210@gmail.com>
2023-01-24 15:29:32 -0300

    Post para Cadastrar funcionarios

--- src/main/java/com/example/demo/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/controller/FuncionarioController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Funcionario;
@@ -20,6 +22,11 @@ public class FuncionarioController {
         return repository.findById(Long.parseLong(id))
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/funcionario/cadastrar")
+    public Funcionario Cadastrar(@RequestBody Funcionario funcionario){
+        return repository.save(funcionario);
     }
 
 }
