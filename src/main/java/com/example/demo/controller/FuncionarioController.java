35cc04d229081b38b83225a046347361567eda0b
nathan ks <nathanks0210@gmail.com>
2023-01-24 17:21:48 -0300

    Concertando verbos rest

--- src/main/java/com/example/demo/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/controller/FuncionarioController.java
@@ -3,11 +3,15 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Funcionario;
@@ -27,18 +31,19 @@ public class FuncionarioController {
     }
 
     @PostMapping(path = "/api/funcionario/cadastrar")
+    @ResponseStatus(HttpStatus.CREATED)
     public Funcionario Cadastrar(@RequestBody Funcionario funcionario){
         return repository.save(funcionario);
     }
 
     // apesar das duas fonções serem iguias, criei duas para garantir que o usuarios não se confulda
     // para atualizar um funcionario, é necessario se certificar do id do funcionario que esta sendo atualizado
-    @PostMapping(path = "/api/funcionario/atualizar")
+    @PutMapping(path = "/api/funcionario/atualizar")
     public Funcionario Atualizar(@RequestBody Funcionario funcionario){
         return repository.save(funcionario);
     }
 
-    @GetMapping(path = "/api/funcionario/deletar/{id}")
+    @DeleteMapping(path = "/api/funcionario/deletar/{id}")
     public ResponseEntity<Funcionario> Deletar(@PathVariable("id") long id){
         return repository.findById(id)
                 .map(record -> {
